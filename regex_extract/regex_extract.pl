496260597eb139162386428587cd021cc86a5e60
climby <mdengfeng@gmail.com>
2015-02-14 19:04:24 +0800

    Extract the transaction code

--- regex_extract/regex_extract.pl
+++ regex_extract/regex_extract.pl
@@ -7,60 +7,60 @@ use utf8;
 
 my $input = $ARGV[0];
 
-
-my ( $date, $time, $transaction, $amount, $agent_number, $agent_name,
+my ( $code, $date, $time, $transaction, $amount, $agent_number, $agent_name,
 	$balance );
 
 if ( $input =~
-/Confirmed\.\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+(Withdraw)\s+[^\d]+([\.,\d]+)\s+from\s+(\d+)\s+-\s([^\n]+).*balance\s+is[^\d]+([\.,\d]+)/msi
+/(.*)\s+Confirmed\.\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+(Withdraw)\s+[^\d]+([\.,\d]+)\s+from\s+(\d+)\s+-\s([^\n]+).*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
 	(
-		$date, $time, $transaction, $amount, $agent_number, $agent_name,
+		$code, $date, $time, $transaction, $amount, $agent_number, $agent_name,
 		$balance
-	) = ( $1, $2, $3, $4, $5, $6, $7 );
+	) = ( $1, $2, $3, $4, $5, $6, $7, $8 );
 }
 elsif ( $input =~
-/Confirmed\.\s+.*(received)\s+[^\d]+([\.,\d]+)\s+from\s+([^\d]+)\s+(\d+)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+.*balance\s+is[^\d]+([\.,\d]+)/msi
+/(.*)\s+Confirmed\.\s+.*(received)\s+[^\d]+([\.,\d]+)\s+from\s+([^\d]+)\s+(\d+)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+.*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
 	(
-		$transaction, $amount, $agent_name, $agent_number, $date, $time,
+		$code, $transaction, $amount, $agent_name, $agent_number, $date, $time,
 		$balance
-	) = ( $1, $2, $3, $4, $5, $6, $7 );
+	) = ( $1, $2, $3, $4, $5, $6, $7, $8 );
 }
 elsif ( $input =~
-/Confirmed\.\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+(Give)\s+[^\d]+([\.,\d]+)\s+.*to\s+([^\n]+).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
+/(.*)\s+Confirmed\.\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+(Give)\s+[^\d]+([\.,\d]+)\s+.*to\s+([^\n]+).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
-	( $date, $time, $transaction, $amount, $agent_name, $balance ) =
-	  ( $1, $2, $3, $4, $5, $6 );
+	( $code, $date, $time, $transaction, $amount, $agent_name, $balance ) =
+	  ( $1, $2, $3, $4, $5, $6, $7 );
 }
 elsif ( $input =~
-/Confirmed\.\s+.*(bought)\s+[^\d]+([\.,\d]+)\s+.*of\s+(.*)\s+(?:for)?\s*(\d*)\s*on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*New.*balance\s+is[^\d]+([\.,\d]+).*/msi
+/(.*)\s+Confirmed\.\s+.*(bought)\s+[^\d]+([\.,\d]+)\s+.*of\s+(.*)\s+(?:for)?\s*(\d*)\s*on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*New.*balance\s+is[^\d]+([\.,\d]+).*/msi
   )
 {
 	(
-		$transaction, $amount, $agent_name, $agent_number, $date, $time,
+		$code, $transaction, $amount, $agent_name, $agent_number, $date, $time,
 		$balance
-	) = ( $1, $2, $3, $4, $5, $6, $7 );
+	) = ( $1, $2, $3, $4, $5, $6, $7, $8 );
 }
-elsif ( $input =~
-/Confirmed\.\s+[^\d]+([\.,\d]+)\s+(sent)\s+to\s+([^\d]+)\s+(\d+)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
+elsif (
+	$input =~
+/(.*)\s+Confirmed\.\s+[^\d]+([\.,\d]+)\s+(sent)\s+to\s+([^\d]+)\s+(\d+)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
 	(
-		$amount, $transaction, $agent_name, $agent_number, $date, $time,
+		$code, $amount, $transaction, $agent_name, $agent_number, $date, $time,
 		$balance
-	) = ( $1, $2, $3, $4, $5, $6, $7 );
+	) = ( $1, $2, $3, $4, $5, $6, $7, $8 );
 }
 elsif ( $input =~
-/Confirmed\..*repaid\s+(loan)\s+[^\d]+([\.,\d]+)\s+from\s+(.*)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*balance\s+is[^\d]+([\.,\d]+).*loan/msi
+/(.*)\s+Confirmed\..*repaid\s+(loan)\s+[^\d]+([\.,\d]+)\s+from\s+(.*)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*balance\s+is[^\d]+([\.,\d]+).*loan/msi
   )
 {
-	( $transaction, $amount, $agent_name, $date, $time, $balance ) =
-	  ( $1, $2, $3, $4, $5, $6 );
+	( $code, $transaction, $amount, $agent_name, $date, $time, $balance ) =
+	  ( $1, $2, $3, $4, $5, $6, $7 );
 }
 
-
-print "$date,$time,$transaction,$amount,$agent_number,$agent_name,$balance\n";
+print
+  "$code,$date,$time,$transaction,$amount,$agent_number,$agent_name,$balance\n";
