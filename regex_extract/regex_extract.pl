3b1532053f7a2a7f524b1610342217830e60996a
climby <mdengfeng@gmail.com>
2015-02-14 20:52:07 +0800

    Add transaction type

--- regex_extract/regex_extract.pl
+++ regex_extract/regex_extract.pl
@@ -7,13 +7,16 @@ use utf8;
 
 my $input = $ARGV[0];
 
-my ( $code, $date, $time, $transaction, $amount, $agent_number, $agent_name,
-	$balance );
+my (
+	$type,   $code,         $date,       $time, $transaction,
+	$amount, $agent_number, $agent_name, $balance
+);
 
 if ( $input =~
 /(.*)\s+Confirmed\.\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+(Withdraw)\s+[^\d]+([\.,\d]+)\s+from\s+(\d+)\s+-\s([^\n]+).*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
+	$type = 'Agent Withdraw';
 	(
 		$code, $date, $time, $transaction, $amount, $agent_number, $agent_name,
 		$balance
@@ -23,6 +26,7 @@ elsif ( $input =~
 /(.*)\s+Confirmed\.\s+.*(received)\s+[^\d]+([\.,\d]+)\s+from\s+([^\d]+)\s+(\d+)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+.*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
+	$type = 'Receive';
 	(
 		$code, $transaction, $amount, $agent_name, $agent_number, $date, $time,
 		$balance
@@ -32,6 +36,7 @@ elsif ( $input =~
 /(.*)\s+Confirmed\.\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)\s+(Give)\s+[^\d]+([\.,\d]+)\s+.*to\s+([^\n]+).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
+	$type = 'Agent Deposit';
 	( $code, $date, $time, $transaction, $amount, $agent_name, $balance ) =
 	  ( $1, $2, $3, $4, $5, $6, $7 );
 }
@@ -44,11 +49,21 @@ elsif ( $input =~
 		$balance
 	) = ( $1, $2, $3, $4, $5, $6, $7, $8 );
 }
-elsif (
-	$input =~
+elsif ( $input =~
 /(.*)\s+Confirmed\.\s+[^\d]+([\.,\d]+)\s+(sent)\s+to\s+([^\d]+)\s+(\d+)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
   )
 {
+	$type = 'Send';
+	(
+		$code, $amount, $transaction, $agent_name, $agent_number, $date, $time,
+		$balance
+	) = ( $1, $2, $3, $4, $5, $6, $7, $8 );
+}
+elsif ( $input =~
+/(.*)\s+Confirmed\.\s+[^\d]+([\.,\d]+)\s+(sent)\s+to\s+(.*)\s+for\s+(.*)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*New.*balance\s+is[^\d]+([\.,\d]+)/msi
+  )
+{
+	$type = 'Paybill';
 	(
 		$code, $amount, $transaction, $agent_name, $agent_number, $date, $time,
 		$balance
@@ -58,9 +73,27 @@ elsif ( $input =~
 /(.*)\s+Confirmed\..*repaid\s+(loan)\s+[^\d]+([\.,\d]+)\s+from\s+(.*)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*balance\s+is[^\d]+([\.,\d]+).*loan/msi
   )
 {
-	( $code, $transaction, $amount, $agent_name, $date, $time, $balance ) =
+
+	# ignore this type;
+	#( $code, $transaction, $amount, $agent_name, $date, $time, $balance ) =
+	#  ( $1, $2, $3, $4, $5, $6, $7 );
+}
+elsif ( $input =~
+/(.*)\s+Confirmed\.\s+[^\d]+([\.,\d]+)\s+(paid)\s+to\s+(.*)\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M).*balance\s+is[^\d]+([\.,\d]+)/msi
+  )
+{
+	$type = 'Buy goods';
+	( $code, $amount, $transaction, $agent_name, $date, $time, $balance ) =
+	  ( $1, $2, $3, $4, $5, $6, $7 );
+}
+elsif ( $input =~
+/(.*)\s+Confirmed\.?\s+on\s+([\/\d]+)\s+at\s+([\d:]+\s+[AP]M)[^\d]+([\.,\d]+)\s+(withdrawn)\s+from\s+([\d]+)\s+-\s+AGENT\s+ATM.*balance\s+is[^\d]+([\.,\d]+)/msi
+  )
+{
+	$type = "Atm Withdraw";
+	( $code, $date, $time,, $amount, $transaction, $agent_number, $balance ) =
 	  ( $1, $2, $3, $4, $5, $6, $7 );
 }
 
 print
-  "$code,$date,$time,$transaction,$amount,$agent_number,$agent_name,$balance\n";
+"$type,$code,$date,$time,$transaction,$amount,$agent_number,$agent_name,$balance\n";
